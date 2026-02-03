0ce911c6bb2ade6b7e4a1b75bfcf915f163af6be
hugomurilloo <hugomurillocaparros@gmail.com>
2026-02-03 20:09:16 +0100

    Final

--- app/src/main/java/com/example/examenhugo/MainActivity.kt
+++ app/src/main/java/com/example/examenhugo/MainActivity.kt
@@ -1,6 +1,4 @@
 package com.example.examenhugo
-
-import android.R
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_main)
     }
 }

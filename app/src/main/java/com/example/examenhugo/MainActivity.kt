5813d620cc0325b411eb0a189a5e051fc9fe9cef
hugomurilloo <hugomurillocaparros@gmail.com>
2026-02-03 20:04:03 +0100

    Fragment funcional

--- app/src/main/java/com/example/examenhugo/MainActivity.kt
+++ app/src/main/java/com/example/examenhugo/MainActivity.kt
@@ -1,16 +1,14 @@
 package com.example.examenhugo
 
+import android.R
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
 
     }
 }
